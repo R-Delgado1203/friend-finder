7ea1b022478cb1f3702e3c5a7d17159bc1b1ff58
RolandoD <r.delgado1203@gmail.com>
2018-10-30 05:18:12 -0700

    updated server.js to point to correct path

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ var PORT = 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
 
 app.listen(PORT, () => {
     console.log("App listening on PORT: " + PORT);

79d34bd4e9dd941079bd3520e4e785566898fab8
R-Delgado1203 <r.delgado1203@gmail.com>
2018-10-31 19:30:27 -0700

    finalized app upload

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# friend-finder
+# **friend-finder**
+
+Friend-Finder app allows you to take a survey and find the best possible match for a "friend" from a list of other users that have taken the survey. 
+
+After completing the survey the app will match the current user with a previous user based on the answers selected in the survey for compatibility, and will display the users name and image if one was provided.
+
+Find the deployed app here. https://r-delgado1203-friend-finder.herokuapp.com/
+
+![alt text](./images/home.png "friend-finder")
+
+![alt text](./images/fullapp.png "friend-finder")
+
+![alt text](./images/match.png "match")
--- app/data/friends.js
+++ app/data/friends.js
@@ -1,2 +1,67 @@
-let friends = [];
+let friends = [
+    {
+        name: "Marty Mcfly",
+        photo: "https://vignette.wikia.nocookie.net/bttf/images/e/e5/Martyvest1955.jpg/revision/latest/scale-to-width-down/250?cb=20110112223247",
+        scores: [
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "5",
+            "4",
+            "3",
+            "2",
+            "1"
+        ]
+    },
+    {
+        name: "Doc Brown",
+        photo: "https://upload.wikimedia.org/wikipedia/en/thumb/9/97/Doc_Brown.JPG/220px-Doc_Brown.JPG",
+        scores: [
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5"
+        ]
+    },
+    {
+        name: "Rolando",
+        photo: "https://scontent.fphx1-1.fna.fbcdn.net/v/t1.0-9/26165137_10155760046890801_1966025897905151675_n.jpg?_nc_cat=102&_nc_ht=scontent.fphx1-1.fna&oh=1884a10bc25e9f55754979028059bea2&oe=5C3BE3E1",
+        scores: [
+            "4",
+            "5",
+            "2",
+            "3",
+            "5",
+            "2",
+            "5",
+            "2",
+            "3",
+            "1"
+        ]
+    },
+    {
+        name: "Reanna",
+        photo: "https://media.licdn.com/dms/image/C5103AQEYbvhb7oRS2A/profile-displayphoto-shrink_800_800/0?e=1542844800&v=beta&t=TISjxGQy_JQdHv9wlH2Ve5qXO09SbOIuTBVogKCTW_0",
+        scores: [
+            "3",
+            "4",
+            "3",
+            "2",
+            "2",
+            "1",
+            "1",
+            "5",
+            "5",
+            "2"
+        ]
+    },
+];
 module.exports = friends;
